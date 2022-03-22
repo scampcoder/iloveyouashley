fef0bf656d23221916da8f5d74d8e390dcceb4e7
Victoria Kee <victoriakee95@gmail.com>
2022-03-22 17:18:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,107 @@
+const compliments = [
+    {
+        id: 1,
+        text: "My beautiful goddess, I hope today is as special and magical as you are."
+    },
+    {
+        id: 2,
+        text: "Hey bish. Whatever you're going through right now, you're gonna do great. You're amazing and I love you so much."
+    },
+    {
+        id: 3,
+        text: "Sweet baby Jesus, you got a beautiful face!"
+    },
+    {
+        id: 4,
+        text: `Hey lady, guess what... I love you!`
+    },
+    {
+        id: 5,
+        text: "If you ever have moments when you doubt yourself. Think about what an absolute badass you are. You have gone through so much and continue remain completely kickass."
+    },
+    {
+        id: 6,
+        text: "Member that time we went to Colorado and climbed a damn mountain? That was cool. Majestic bugles."
+    },
+    {
+        id: 7,
+        text: "I think you are the most amazing person I've ever planned to be a golden girl with. Rocking chairs and knitting, here we come."
+    },
+    {
+        id: 8,
+        text: "Cheers to the smartest girl I know."
+    },
+    {
+        id: 9,
+        text: "Hey you. You're doing amazing today. (And you look really hot too!)"
+    },
+    {
+        id: 10,
+        text: "Here's a picture of a lumberjack to brighten your day:",
+        image: "https://i.pinimg.com/originals/a1/1c/ef/a11cefd353e434f5a43d9a4d3a0301aa.jpg",
+        alt: "Hot beefy lumberjack"
+    },
+    {
+        id: 11,
+        text: "You're simply the breast! I mean best. You're the tits, lady."
+    },
+    {
+        id: 12,
+        text: "Psssst. You're incredible. Like honestly, completely stunning. Amazing. Smart. 10/10"
+    },
+    {
+        id: 13,
+        text: "If you're feeling down, please remember that you are a wonderful person and lots of people love you and think you're awesome."
+    },
+    {
+        id: 14,
+        text: "Oh my gosh! I didn't know I was gonna get to see a queen today! Damn. *Chef's kiss*"
+    },
+    {
+        id: 15,
+        text: "Good day, m'lady. Might I cheer you up with a photo of a puppy making a friend?",
+        image: "https://preview.redd.it/dmz6rp5peuj11.jpg?auto=webp&s=c81b139498ddcb4e79c84a9a524d7de0405bf1a2",
+        alt: "Cute puppy with ladybug on it's nose"
+    },
+    {
+        id: 16,
+        text: "You should be incredibly proud of all that you've accomplished."
+    },
+    {
+        id: 17,
+        text: "You're all that and a super-size bag of chips."
+    },
+    {
+        id: 18,
+        text: "What's a six letter word for someone who's the coolest girl ever? Oh I know! A-S-H-L-E-Y."
+    },
+    {
+        id: 19,
+        text: "You're simply the breast! I mean best. You're the tits, lady."
+    },
+    {
+        id: 20,
+        text: "If you were a box of crayons, you'd be the giant name-brand one with the built-in sharpener."
+    },
+]
+
+const complimentTxt = document.getElementById("compliment");
+const image = document.createElement('img');
+const complimentBox = document.getElementById('inner-compliment');
+
+function getCompliment() {
+    const randomIndex = Math.floor(Math.random() * compliments.length)
+    const compliment = compliments[randomIndex];
+        if(compliment.hasOwnProperty('image')) {
+            console.log("image found")
+            complimentTxt.innerHTML = compliment.text
+            image.src = compliment.image;
+            image.className = "image col-6";
+            complimentBox.appendChild(image)
+        } else {
+            console.log("image not found")
+            complimentTxt.innerHTML = compliment.text
+        }
+    }
+
+getCompliment()
